1422c6357ded8f029c775b545b39066dbcdc10f3
erdemkaa <erdemkaa@gmail.com>
2025-06-20 18:59:47 +0200

    finalfinal

--- static/secret.js
+++ static/secret.js
@@ -2,4 +2,4 @@
 // Developers say you shouldn't look too closely here...
 
 let secret = atob("ZmxhZ3tqczFzX2Nhbl9saWUhfQ=="); // base64 → flag{js1s_can_lie!}
-console.log("Debug Info: " + secret);
+// console.log("Debug Info: " + secret);
